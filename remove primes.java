baed95d77ae54b424da19c2dc3b474ae25d6d8d0
Khushi <68914425+KhushiSri12@users.noreply.github.com>
2021-12-04 12:57:28 +0530

    Add files via upload

---
+++ remove primes.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static boolean isPrime(int v){
+        for(int div=2;div*div<=v;div++){
+            if(v%div==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+	public static void solution(ArrayList<Integer> a1){
+		// write your code here
+		for(int i=a1.size()-1;i>=0;i--){
+		    int v=a1.get(i);
+		    if(isPrime(v)==true){
+		        a1.remove(i);
+		    }
+		}
+		
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i = 0 ; i < n; i++){
+			al.add(scn.nextInt());
+		}
+		solution(al);
+		System.out.println(al);
+	}
+
+}
